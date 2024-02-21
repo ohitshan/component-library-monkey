5a1b643d00b2fd9b02dd3899ebfd7ba15738ed6d
ohitshan <rkddkwl1364@gmail.com>
2024-02-22 01:36:03 +0900

    feat:styled-components setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import Image from "next/image";
+import StyledComponentsRegistry from "./lib/registry";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,30 +20,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className="p-[12px] border-b-[1px] border-b-[#FFFFFF] mb-[24px] flex items-center justify-between">
-          <Link href={"/"}>
-            <Image
-              className="cursor-pointer"
-              src="/monkey.jpeg"
-              width={30}
-              height={30}
-              alt="Picture of the author"
-            />
-          </Link>
-          <ul className="flex gap-[16px]">
-            <Link href={"/installation"}>
-              <li className="hover:scale-[1.1] hover:cursor-pointer hover:text-yellow-400">
-                Installation
-              </li>
+        <StyledComponentsRegistry>
+          <header className="p-[12px] border-b-[1px] border-b-[#FFFFFF] mb-[24px] flex items-center justify-between">
+            <Link href={"/"}>
+              <Image
+                className="cursor-pointer"
+                src="/monkey.jpeg"
+                width={30}
+                height={30}
+                alt="Picture of the author"
+              />
             </Link>
-            <Link href={"/components"}>
-              <li className="hover:scale-[1.1] hover:cursor-pointer hover:text-yellow-400">
-                Components
-              </li>
-            </Link>
-          </ul>
-        </header>
-        {children}
+            <ul className="flex gap-[16px]">
+              <Link href={"/installation"}>
+                <li className="hover:scale-[1.1] hover:cursor-pointer hover:text-yellow-400">
+                  Installation
+                </li>
+              </Link>
+              <Link href={"/components"}>
+                <li className="hover:scale-[1.1] hover:cursor-pointer hover:text-yellow-400">
+                  Components
+                </li>
+              </Link>
+            </ul>
+          </header>
+          {children}
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
